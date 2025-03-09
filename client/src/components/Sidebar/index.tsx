f61d87cfe41d41652c2d73ce2c9c2df70f04d388
Marlon Lima <marlonlima984@gmail.com>
2025-03-09 17:30:46 -0300

    feat: update Sidebar component with logo size adjustments and add new image asset

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -53,8 +53,8 @@ const Sidebar = () => {
           )}
         </div>
         {/* Sidebar Body */}
-        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
-          <Image src="/logo.png" alt="Logo" width={40} height={40} />
+        <div className="flex items-center gap-5 border-y-[1.5px] px-8 py-4 dark:border-gray-700">
+          <Image src="/logo.png" alt="Logo" width={50} height={45} style={{ width: "auto" }} priority/>
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
               ML TEAM
