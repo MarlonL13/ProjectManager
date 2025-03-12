4dd2a25f06d77e33ecfa00b8272f3040a4f828e7
Marlon Lima <marlonlima984@gmail.com>
2025-03-12 09:04:57 -0300

    feat: add user creation endpoint to user controller and update routes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/userControllers";
+import { getUsers, postUser } from "../controllers/userControllers";
 
 const router = Router();
 
 router.get("/", getUsers);
+router.post("/", postUser);
 
 export default router;
